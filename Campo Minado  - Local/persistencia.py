577c37d61abcbed174bd98cd3639a6699085192c
diego teixeira marques <1626301>
2018-10-30 20:36:39 -0300

    Implementacao serverThread

--- Campo Minado  - Local/persistencia.py
+++
@@ -1,8 +0,0 @@
-class Salvar:
-    def __init__(self, linha, coluna, nJogadas, campoAtual):
-        self.linha = linha
-        self.coluna = coluna
-        self.nJogadas = nJogadas
-        self.campoAtual = campoAtual
-
-#Não concluído, falta adicionar as informações do jogo em um arquivo
